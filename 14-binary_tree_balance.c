58e83255e0e3e4b56c80c91977fee80dc382ee51
phareedahadamu <phareedahadamu@gmail.com>
2024-05-01 00:28:49 +0100

    Task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left)
+		l = 1 + tree_height(tree->left);
+	else
+		l = 1;
+	if (tree->right)
+		r = 1 + tree_height(tree->right);
+	else
+		r = 1;
+	if (l > r)
+		return (l);
+	else
+		return (r);
+}
+
+/**
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right, left;
+
+	if (tree)
+	{
+		left = tree_height(tree->left);
+		right = tree_height(tree->right);
+	}
+	return (left - right);
+}
