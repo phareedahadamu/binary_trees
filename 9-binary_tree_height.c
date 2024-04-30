70006c066de8e9179223f3f08cc04564a1fd99b6
phareedahadamu <phareedahadamu@gmail.com>
2024-04-30 17:59:17 +0100

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,10 +7,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
 	size_t lh, rh;
 
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 	lh = binary_tree_height(tree->left);
 	rh = binary_tree_height(tree->right);
 	if (lh >= rh)
